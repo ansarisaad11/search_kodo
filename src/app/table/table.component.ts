566ca0bd9df1bc5a37152a19cf449f60ce7c0284
Saad Ansari <saadansari@Saads-MacBook-Air.local>
2020-11-29 20:42:58 +0530

    added table on home component

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { SearchModel } from "../models/search.model";
 
 @Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  selector: "app-table",
+  templateUrl: "./table.component.html",
+  styleUrls: ["./table.component.scss"],
 })
 export class TableComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() tableData: Array<SearchModel> = [];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
